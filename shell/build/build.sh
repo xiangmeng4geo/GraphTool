e5adb5044df7a64cf3a9355aa1f66c91796b425c
tonny <wodexintiao@gmail.com>
2015-03-15 00:47:15 +0800

    添加生成脚本，并对文件后缀及文件内容进行处理

---
+++ shell/build/build.sh
@@ -0,0 +1,9 @@
+VERSION=V0.1
+PROJECT_NAME=GraphTool
+target_dir=./target/${PROJECT_NAME}_${VERSION}
+
+#1. compress files
+node ../../../compresser/ ../../core/ $target_dir/
+
+#2. change suffix
+node ./change_suffix.js $target_dir
---
+++ shell/build/change_suffix.js
@@ -0,0 +1,52 @@
+var fs = require('fs'),
+	path = require('path');
+
+var TRANSLATION = {
+	'.html': '.gt',
+	'.js': '.gts',
+	'.css': '.gtc'
+};
+function changeSuffix(dir){
+	var files = fs.readdirSync(dir);
+	files.forEach(function(file){
+		var pathname = path.join(dir, file),
+			stat = fs.lstatSync(pathname);
+		
+		if(stat.isDirectory()){
+			if(file == 'css' || file == 'js'){
+				if(file == 'css'){
+					file = 'c';
+				}else{
+					file = 'j';
+				}
+				var new_pathname = path.join(dir, file);
+				fs.renameSync(pathname, new_pathname);
+				changeSuffix(new_pathname);
+			}else{
+				changeSuffix(pathname);
+			}
+		}else{
+			var ext = path.extname(pathname);
+			var toExt = TRANSLATION[ext];
+			if(toExt){
+				var newPathName = path.join(dir, path.basename(pathname, ext) + toExt);
+				newPathName = newPathName;
+				fs.renameSync(pathname, newPathName);
+				if(ext == '.html'){
+					var content = fs.readFileSync(newPathName, 'utf8');
+					content = content.replace(/\.css/g,'.gtc').replace(/css(?=\/)/g,'c')
+						.replace(/\.js/g,'.gtj').replace(/js(?=\/)/g,'j');
+					fs.writeFileSync(newPathName, content);
+				}
+			}
+		}
+	});
+}
+
+
+var args = [].slice.call(process.argv);
+//命令行进行指定文件压缩
+if(args.length > 2){
+	var dir = args[2];
+	changeSuffix(dir);
+}
