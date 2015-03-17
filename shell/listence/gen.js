ee829beedf2f46582e4fe8901776a593e17c3064
tonny <wodexintiao@gmail.com>
2015-03-17 09:52:20 +0800

    添加有效期序列号

---
+++ shell/listence/gen.js
@@ -0,0 +1,30 @@
+var fs = require('fs');
+var path = require('path');
+var core = require('../../core/node_modules/core');
+var util = core.util;
+
+String.prototype.reverse = function(){
+	return this.split('').reverse().join('');
+}
+function gen(time_start, time_end, is_from_command){
+	time_start = new Date(time_start),
+	time_end = new Date(time_end);
+	var source_str = [time_start.getTime(), time_end.getTime()].join('|');
+	var listen_str = util.encrypt.encode(source_str);
+	listen_str = listen_str.reverse().toUpperCase();
+	if(is_from_command){
+		return source_str+'\t'+listen_str+'\n\r';
+	}else{
+		return listen_str;
+	}
+}
+var args = [].slice.call(process.argv);
+//命令行进行指定文件压缩
+if(args.length >= 2){
+	var time_start = args[2] || '2015-03-17',
+		time_end = args[3] || '2015-04-20';
+	fs.appendFileSync(path.join(__dirname, './listence.txt'), gen(time_start, time_end, true));
+}
+
+
+exports.gen = gen;
