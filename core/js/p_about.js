d6d5c503e4bb3464c04a3fe75062c6033d35a223
tonny <wodexintiao@gmail.com>
2015-04-21 18:26:29 +0800

    修改

--- core/js/p_about.js
+++ core/js/p_about.js
@@ -1,5 +1,5 @@
 Core.safe(function(){
-	Core.Window.get().showDevTools();
+	// Core.Window.get().showDevTools();
 
 	var info = Core.appInfo;
 	$('#version').text(info.name + ' ('+info.version+')');
