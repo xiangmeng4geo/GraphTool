773d017ece308feedf7655618f9075bbd2291353
tonny <wodexintiao@gmail.com>
2016-03-14 18:16:09 +0800

    完成导出功能

--- shell/testdata/source/export.js
+++ shell/testdata/source/export.js
@@ -18,12 +18,71 @@
 		var util_ui_checked = util.ui.checked;
 		var _alert = util.ui.dialog.alert;
 
+		var path_user = path.join(require('os').homedir(), 'BPA', 'GT');
+		var path_config = path.join(path_user, 'config');
+
 		var PATH_DATA = path.join(__dirname, 'data');
 		var PATH_DATA_CONFIG = path.join(PATH_DATA, 'config');
+		var PATH_DATA_DATA = path.join(PATH_DATA, 'data');
 		var PATH_DATA_GEOFILE = path.join(PATH_DATA, 'geo');
 
+		var $log = $('#log'),
+			$logTextarea = $log.find('textarea');
 		function _log(msg) {
+			$logTextarea.val($logTextarea.val()+'\n'+msg);
 			console.log(msg);
+		}
+		function _getNewest(dir, rule){
+			var time_start,time_end;
+			var args = arguments;
+			var file;
+			if(args.length == 3){
+				var days = args[2];
+				time_end = new Date();
+				time_start = new Date();
+				time_start.setDate(time_start.getDate() - days);
+			}else{
+				time_start = new Date(args[2]),
+				time_end = new Date(args[3]);
+			}
+
+			for(; time_start <= time_end; ){
+				var file_name = time_end.format(rule);
+				var file_path = path.join(dir, file_name);
+				if(util.file.exists(file_path)){
+					var mtime = fs.statSync(file_path).mtime;
+					if(mtime >= time_start){
+						return file_path;
+					}
+				}
+				time_end.setDate(time_end.getDate() - 1);
+			}
+		}
+		function _parse(data) {
+			var data_conf = data.val;
+			var dir_in = data_conf.dir_in;
+			var param = [dir_in];
+
+			var file_rule = data_conf.file_rule;
+			var is_common = file_rule.is_common;
+			
+			if (is_common) {
+				var val_file_rule = file_rule.val_common || {};
+				param.push(val_file_rule.prefix + val_file_rule.date_format + val_file_rule.postfix + '.' + val_file_rule.file_suffix);
+			} else {
+				param.push(file_rule.val_custom);
+			}
+			var file = data_conf.file;
+			var is_newest = file.is_newest;
+			var val_is_newest = file.val;
+			if (is_newest) {
+				param.push(val_is_newest.newest_days);
+			} else {
+				param.push(val_is_newest.start);
+				param.push(val_is_newest.end);
+			}
+
+			return _getNewest.apply(null, param);
 		}
 		function _isChecked(obj) {
 			if (obj.state.selected) {
@@ -68,7 +127,46 @@
 		}
 		function _exportProduct() {
 			var result = _getAllChecked(tree_product);
-			return util.file.write(path.join(PATH_DATA_CONFIG, '.tree.json'), JSON.stringify(result));
+			function _copy(arr) {
+				arr.forEach(function(v) {
+					var children = v.childNodes;
+					if (children) {
+						_copy(children);
+					} else {
+						try {
+							var conf = require(path.join(path_config, v.name));
+						} catch(e){
+						}
+						if (conf) {
+							var conf_val = conf.data.val;
+							conf_val.file = {
+								is_newest: true,
+								val: {
+									newest_days: 999
+								}
+							}
+
+							var file_path = null;
+							try {
+								file_path = _parse(conf.data);
+							} catch(e){}
+							if (util.file.exists(file_path)) {
+								var file_name = path.basename(file_path);
+								util.file.copy(file_path, path.join(PATH_DATA_DATA, file_name));
+
+								_log('\t复制'+file_path);
+
+								conf_val.file_rule.is_common = false;
+								conf_val.file_rule.val_custom = file_name;
+							}
+							conf_val.dir_in = '';
+							_log('成功处理'+v.name);
+						}
+					}
+				});
+			}
+			_copy(result);
+			util.file.write(path.join(PATH_DATA_CONFIG, '.tree.json'), JSON.stringify(result));
 		}
 		function _exportMap() {
 			var result = _getAllChecked(tree_map);
@@ -79,20 +177,20 @@
 			});
 			data_sys_geo.forEach(function(map) {
 				if (cache[map.name]) {
-					var n_exists = 0;
-					var n_len = map.maps.length;
+					var maps_new = [];
 					map.maps.forEach(function(map_file) {
 						var file = map_file.file;
 						var file_name = path.basename(file);
 						if (util.file.exists(file)) {
 							util.file.copy(file, path.join(PATH_DATA_GEOFILE, file_name));
 							map_file.file = file_name;
-							n_exists++;
+							maps_new.push(map_file);
 						} else {
 							_log(file+'不存在！');
 						}						
 					});
-					if (n_exists > 0) {
+					if (maps_new.length > 0) {
+						map.maps = maps_new;
 						data_map.push(map);		
 					}
 				}
@@ -101,7 +199,20 @@
 			_log('成功导出'+data_map.length+'条地图配置！');
 		}
 		function _exportLegend() {
-			
+			var result = _getAllChecked(tree_legend);
+			var data = [];
+			var cache = {};
+			result.forEach(function(map) {
+				cache[map.name] = 1;
+			});
+			data_sys_legend.forEach(function(legend) {
+				if (cache[legend.name]) {
+					data.push(legend);
+				}
+			});
+
+			util.file.write(path.join(PATH_DATA_CONFIG, '.legend.json'), JSON.stringify(data));
+			_log('成功导出'+data.length+'条地图配置！');
 		}
 
 		var treeDataProduct = [{
