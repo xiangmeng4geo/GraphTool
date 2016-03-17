93a518393cf0682edfdbb2365a6fec7b9e924ff7
tonny <wodexintiao@gmail.com>
2016-03-17 10:35:43 +0800

    a

---
+++ shell/testdata/target/export/resources/app/lib/asar_stream/chromium-pickle-js.js
@@ -0,0 +1,1 @@
+(function(){var e;e=require("./pickle"),module.exports={createEmpty:function(){return new e},createFromBuffer:function(r){return new e(r)}}}).call(this);
---
+++ shell/testdata/target/export/resources/app/lib/asar_stream/cuint/index.js
@@ -0,0 +1,1 @@
+exports.UINT32=require("./lib/uint32"),exports.UINT64=require("./lib/uint64");
